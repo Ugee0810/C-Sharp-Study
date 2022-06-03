ba5efcb085447c550bacda4a16497c03abe59631
shu990810@naver.com <shu990810@naver.com>
2022-06-03 12:19:36 +0900

    2022/06/03 4교시

--- Ch03/1_If.cs
+++ Ch03/1_If.cs
@@ -7,12 +7,67 @@ using System.Threading.Tasks;
 /*
  * 날짜 : 2022/06/03
  * 이름 : 송환욱
- * 내용 : 
+ * 내용 : 조건문 실습 교재 p127
  */
 
 namespace Ch03
 {
     internal class _1_If
     {
+        static void Main1(string[] args)
+        {
+            // if
+            int num1 = 1;
+            int num2 = 2;
+
+            if (num1 < num2) 
+            {
+                // 조건이 True일 시 실행
+                Console.WriteLine("num1은 num2보다 작다.");
+            }
+
+            if (num1 > 1) // 결과가 false if 실행 X
+            {
+                Console.WriteLine("num1은 1보다 크다.");
+            }
+
+            if (num1 > 0) // 첫 번째 조건
+            {
+                if (num2 > 1) // 두 번째 조건
+                {
+                    Console.WriteLine("num1은 0보다 크고, num2는 1보다 크다.");
+                }
+            }
+            
+            // if ~ else
+            if (num1 > 0)
+            {
+                Console.WriteLine("if ~ else : num1은 0보다 크다.");
+            }
+            else
+            {
+                Console.WriteLine("if ~ else : num1은 0보다 크지 않다.");
+            }
+
+            // if ~ else if ~ else
+            int n1 = 10, n2 = 20, n3 = 30, n4 = 40;
+
+            if (n1 > n2) // 조건 1
+            {
+                Console.WriteLine("n1 > n2");
+            }
+            else if (n2 > n3) // 조건 2
+            {
+                Console.WriteLine("n2 > n3");
+            }
+            else if (n3 > n4) // 조건 3
+            {
+                Console.WriteLine("n3 > n4");
+            }
+            else // false
+            {
+                Console.WriteLine("n4 > n3, n2, n1");
+            }
+        }
     }
 }
--- Ch03/2_Switch.cs
+++ Ch03/2_Switch.cs
@@ -4,9 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * 날짜 : 2022/06/03
+ * 이름 : 송환욱
+ * 내용 : 조건문 Switch 실습 교재 p127
+ */
+
 namespace Ch03
 {
     internal class _2_Switch
     {
+        static void Main(string[] args)
+        {
+            Console.Write("점수 입력 : ");
+            string strscore = Console.ReadLine();
+
+            int score = int.Parse(strscore);
+            int grade = score / 10;
+
+            switch (grade)
+            {
+                case 10: // 100 Score
+                case 9:
+                    Console.WriteLine("A 입니다.");
+                    break;
+                case 8:
+                    Console.WriteLine("B 입니다.");
+                    break;
+                case 7:
+                    Console.WriteLine("C 입니다.");
+                    break;
+                case 6:
+                    Console.WriteLine("D 입니다.");
+                    break;
+                default: // 그 외
+                    Console.WriteLine("F 입니다.");
+                    break;
+            }
+        }
     }
 }
