e7e946db1a70c68ba6aa82f6c1629a0a255e1bd5
shu990810@naver.com <shu990810@naver.com>
2022-06-13 11:13:06 +0900

    2022/06/13 11:13

---
+++ Ch06/2_Interface.cs
@@ -0,0 +1,45 @@
+﻿using Ch06.Sub2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 날짜 : 2022/06/13
+ * 이름 : 송환욱
+ * 내용 : 인터페이스 실습하기 교재 p425
+ */
+
+namespace Ch06
+{
+    internal class _2_Interface
+    {
+        static void Main(string[] args)
+        {
+            //인터페이스로 공통 구조 설계 + 다형성
+            IRemoteContol lg = new LGRemocon();
+            IRemoteContol samsung = new SamsungRemocon();
+
+            lg.PowerOn();
+            lg.PowerOff();
+            lg.ChUp();
+            lg.ChDown();
+            lg.SoundUp();
+            lg.SoundDown();
+
+            samsung.PowerOn();
+            samsung.PowerOff();
+            samsung.ChUp();
+            samsung.ChDown();
+            samsung.SoundUp();
+            samsung.SoundDown();
+
+            //인터페이스를 활용한 다중 상속 -> 다형성 적용은 불가함
+            TV tv = new TV();
+            tv.PowerOn();
+            tv.Booting();
+            tv.Access();
+        }
+    }
+}
