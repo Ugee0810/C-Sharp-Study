6a424728d22d9484a786a179f205531513474bf6
shu990810@naver.com <shu990810@naver.com>
2022-06-13 10:31:27 +0900

    2022/06/13 10:31

---
+++ Ch06/1_AbstractClass.cs
@@ -0,0 +1,40 @@
+﻿using Ch06.Sub1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 날짜 : 2022/06/13
+ * 이름 : 송환욱
+ * 내용 : 추상 클래스 실습하기 교재 p230
+ * 
+ * 추상 클래스(Abstract Class)
+ * - 공통의 기능은 하나의 메서드로 구현하고 개별적인 기능은 상속 받아 override 메서드로 구현시키는 추상 메서드를 갖는 클래스
+ */
+
+namespace Ch06
+{
+    internal class _1_AbstractClass
+    {
+        static void Main(string[] args)
+        {
+            //추상 클래스는 객체생성 안됨
+            //Car car = new Car();
+
+            //Car 다형성의 원리
+            Car sedan = new Sedan("그랜저", "검정색", 0, 2000);
+            Car truck = new Truck("포터", "파란색", 0, 1);
+
+            sedan.SpeedUp(100);
+            sedan.SpeedDown(50);
+            sedan.Show();
+
+            truck.SpeedUp(100);
+            truck.SpeedDown(50);
+            truck.Show();
+
+        }
+    }
+}
---
+++ Ch06/Sub1/Sedan.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch06.Sub1
+{
+    internal class Sedan : Car
+    {
+        //필드
+        private int cc;
+
+        //생성자
+        public Sedan(string name, string color, int speed, int cc) : base(name, color, speed)
+        {
+            this.cc = cc;
+        }
+
+        //Method(기능)
+        public override void SpeedUp(int speed)
+        {
+            Console.WriteLine("Sedan SpeedUp ...");
+            base.speed += speed;
+        }
+
+        public override void SpeedDown(int speed)
+        {
+            Console.WriteLine("Sedan SpeedDown ...");
+            base.speed -= speed;
+        }
+
+        public override void Show()
+        {
+            Console.WriteLine("===============");
+            Console.WriteLine("차랑명 : "     + name);
+            Console.WriteLine("차랑색 : "     + color);
+            Console.WriteLine("차랑속도 : "   + speed);
+            Console.WriteLine("차랑배기량 : " + cc);
+            Console.WriteLine("---------------");
+        }
+    }
+}
