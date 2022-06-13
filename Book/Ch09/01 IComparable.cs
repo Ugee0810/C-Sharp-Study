12d18cf5dce479f49d8320d2c44dc423140e08b3
shu990810@naver.com <shu990810@naver.com>
2022-06-13 13:19:43 +0900

    2022/06/13 13:19

---
+++ Book/Ch09/01 IComparable.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//9-1 IComparable 인터페이스 활용
+//9-1-1(p426) 기본 클래스와 자료 생성
+/*
+namespace Book.Ch09
+{
+    internal class P426
+    {
+        class Product
+        {
+            public string Name { get; set; }
+            public int Price { get; set; }
+
+            public override string ToString()
+            {
+                return Name + " : " + Price + "원";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>()
+            {
+                new Product() { Name = "고구마", Price = 1500},
+                new Product() { Name = "사과"  , Price = 2400},
+                new Product() { Name = "바나나", Price = 1000},
+                new Product() { Name = "배"    , Price = 3000}
+            };
+            list.Sort();
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
+*/
+
+//9-1-2(p427) IComparable 인터페이스 상속
+/*
+namespace Book.Ch09
+{
+    internal class P426
+    {
+        class Product : IComparable
+        {
+            public string Name { get; set; }
+            public int Price { get; set; }
+
+            public override string ToString()
+            {
+                return Name + " : " + Price + "원";
+            }
+        }
+    }
+}
+*/
+
+//9-1-3(p428) Icomparable 인터페이스의 메서드 생성
+/*
+namespace Book.Ch09
+{
+    internal class P426
+    {
+        class Product : IComparable
+        {
+            public string Name  { get; set; }
+            public int    Price { get; set; }
+
+            public override string ToString()
+            {
+                return Name + " : " + Price + "원";
+            }
+
+            public int CompareTo(object? obj)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
+*/
+
+//9-1-4(p429) CompareTo()메서드 구현 CompareTo() -> 규약
+/*
+namespace Book.Ch09
+{
+    internal class P426
+    {
+        class Product : IComparable
+        {
+            public string Name  { get; set; }
+            public int    Price { get; set; }
+
+            public override string ToString()
+            {
+                return Name + " : " + Price + "원";
+            }
+
+            public int CompareTo(object? obj)
+            {
+                return this.Price.CompareTo((obj as Product).Price);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>()
+            {
+                new Product() { Name = "고구마", Price = 1500},
+                new Product() { Name = "사과"  , Price = 2400},
+                new Product() { Name = "바나나", Price = 1000},
+                new Product() { Name = "배"    , Price = 3000}
+            };
+            list.Sort();
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
+*/
+
+//9-1-5(p.429) 인터페이스 인스턴스화
+
+namespace Book.Ch09
+{
+    internal class P426
+    {
+        class Product : IComparable
+        {
+            public string Name { get; set; }
+            public int Price   { get; set; }
+
+            public override string ToString()
+            {
+                return Name + " : " + Price + "원";
+            }
+
+            public int CompareTo(object? obj)
+            {
+                return this.Price.CompareTo((obj as Product).Price);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>()
+            {
+                new Product() { Name = "고구마", Price = 1500},
+                new Product() { Name = "사과"  , Price = 2400},
+                new Product() { Name = "바나나", Price = 1000},
+                new Product() { Name = "배"    , Price = 3000}
+            };
+            list.Sort();
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            
+            //인터페이스는 실체가 없는 규칙이므로 인스턴스화 할 수 없습니다.
+            //IComparable comparable = new IComparable();
+        }
+    }
+}
--- Ch06/2_Interface.cs
+++ Ch06/2_Interface.cs
@@ -9,6 +9,11 @@ using System.Threading.Tasks;
  * 날짜 : 2022/06/13
  * 이름 : 송환욱
  * 내용 : 인터페이스 실습하기 교재 p425
+ * 
+ * 인터페이스(Interface)
+ * - 클래스간의 공통의 통일된 표준 규격을 통해 클래스 구조를 설계하는 문법
+ * - 인터페이스를 활용해서 다중 상속을 구현한다.
+ * - 인터페이스를 활용해서 객체간의 결합도를 완화하는 효과를 얻는다.(다형성)
  */
 
 namespace Ch06
@@ -40,6 +45,14 @@ namespace Ch06
             tv.PowerOn();
             tv.Booting();
             tv.Access();
+
+            //인터페이스를 활용한 결합도 완화 (전구 - 소켓 - 케이블)
+            Bulb bulb = new Bulb();
+            ISocket socket = new Cable(bulb);
+
+            socket.SwitchOn();
+            socket.SwitchOff();
+
         }
     }
 }
