563bca0cbc1f2d108ae866b005f408a9f29e53fc
shu990810@naver.com <shu990810@naver.com>
2022-06-17 12:10:22 +0900

    2022/06/17 12:10

---
+++ Test/4/4_001.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 날짜 : 2022/06/17
+ * 이름 : 송환욱
+ * 내용 : 구조체 연습문제
+ */
+
+namespace Test._4
+{
+    struct Point3D
+    {
+        public int x;
+        public int y;
+        public int z;
+
+        public Point3D(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"{x}, {y}, {z}");
+        }
+    }
+
+    internal class _4_001
+    {
+        static void Main1(string[] args)
+        {
+            Point3D p1;
+            p1.x = 10;
+            p1.y = 20;
+            p1.z = 30;
+
+            Console.WriteLine(p1.ToString());
+
+            Point3D p2 = new Point3D(100, 200, 300);
+            Console.WriteLine(p2.ToString());
+        }
+    }
+}
---
+++ Test/4/4_002.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 날짜 : 2022/06/17
+ * 이름 : 송환욱
+ * 내용 : 분할 클래스 연습문제
+ * partial 사용
+ */
+
+namespace Test._4
+{
+    partial class MyClass
+    {
+        public void Method1()
+        {
+            Console.WriteLine("Method1...");
+        }
+        public void Method2()
+        {
+            Console.WriteLine("Method2...");
+        }
+    }
+
+    partial class MyClass
+    {
+        public void Method3()
+        {
+            Console.WriteLine("Method3...");
+        }
+        public void Method4()
+        {
+            Console.WriteLine("Method4...");
+        }
+    }
+
+    internal class _4_002
+    {
+        static void Main1(string[] args)
+        {
+            MyClass my = new MyClass();
+            
+            my.Method1();
+            my.Method2();
+            my.Method3();
+            my.Method4();
+
+        }
+    }
+}
