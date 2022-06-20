2f6312a69fe1524c458649c043888ba01482c94f
HwanUk Song <shu990810@naver.com>
2022-06-20 13:06:26 +0900

    2022/06/20 13:06

---
+++ Book/Ch10/P470.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book.Ch10
+{
+    internal class P470
+    {
+        //고급 예외 처리
+        static void Main1(string[] args)
+        {
+            Console.Write("입력 : ");
+            string input = Console.ReadLine();
+
+            try
+            {
+                int index = int.Parse(input);
+                Console.WriteLine("입력한 숫자 : " + index);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("예외가 발생했습니다.");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("프로그램이 종료되었습니다.");
+            }
+        }
+    }
+}
--- Programmers/Lv1/42748.cs
+++ Programmers/Lv1/42748.cs
@@ -8,29 +8,29 @@ namespace Programmers.Lv1
 {
     internal class _42748
     {
-        static void Main(string[] args)
+        static void Main1(string[] args)
         {
-            int[] arr = arr[100];
         }
     }
 }
 /*
-using System;
-
-public class Solution {
-    public int[] solution(int[] array, int[,] commands) {
+public class Solution
+{
+    public int[] solution(int[] array, int[,] commands)
+    {
         int[] answer = new int[commands.GetLength(0)];
+
         for (int i = 0; i < commands.GetLength(0); i++)
         {
-            int a = commands[i,0];
-            int b = commands[i,1] + 1;
-            int c = commands[i,2];
-            int [] sub = new int[b-a];
+            int a = commands[i, 0];
+            int b = commands[i, 1] + 1;
+            int c = commands[i, 2];
+            int[] sub = new int[b - a];
 
-            Array.Copy(array,a-1,sub,0,b-a);
+            Array.Copy(array, a-1, sub, 0, b-a);
 
             Array.Sort(sub);
-            answer[i] = sub[c-1];
+            answer[i] = sub[c - 1];
         }
         return answer;
     }
