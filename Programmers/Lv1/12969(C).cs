ada7122265c4f8b6f1b6a5cbb0ce6f33a592bdd8
HwanUk Song <shu990810@naver.com>
2022-06-23 16:00:09 +0900

    2022/06/23 16:00

---
+++ Programmers/Lv1/12969(C).cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmers.Lv1
+{
+    internal class _12969
+    {
+        static void Main1(string[] args)
+        {
+            String[] s;
+
+            Console.Clear();
+            s = Console.ReadLine().Split(' ');
+
+            int a = Int32.Parse(s[0]);
+            int b = Int32.Parse(s[1]);
+
+            Console.WriteLine("{0}", a + b);
+
+            for (int j = 0; j < b; j++)
+            {
+                for (int i = 0; i < a; i++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
