d22d7ea3fcd1549a8d554850a0a01487042b73a0
HwanUk Song <shu990810@naver.com>
2022-06-29 15:53:54 +0900

    22-06-29 15:53

--- Project3/Form1.cs
+++ Project3/Form1.cs
@@ -13,6 +13,10 @@ namespace Project3
         //돌을 구분하기 위한 상태변수
         bool isBlack = true;
 
+        enum STONE { none, black, white };
+        STONE[,] dataSet = new STONE[19, 19];
+
+
         public Form1()
         {
             InitializeComponent();
@@ -40,17 +44,155 @@ namespace Project3
                                           margin + sizeNun * y - sizeNun / 2,
                                           sizeDol,
                                           sizeDol);
-            
+
+            if (dataSet[x, y] != STONE.none)//데이터 x,y 안에 화이트나 블랙이 있다면
+            {
+                MessageBox.Show("해당 좌표엔 이미 돌이 있습니다..");
+                return; // 반환이 없는 메서드를 강제 종료 시킴
+            }
+
             if (isBlack)
             {
-                g.FillEllipse(bBrush, dol);
+                g.FillEllipse(bBrush, dol);  //바둑돌 그리기
+                dataSet[x, y] = STONE.black; //검은색 돌 2차원 배열 좌표 데이터를 해당 배열의 인덱스에 추가
+
                 isBlack = false;
             }
             else
             {
                 g.FillEllipse(wBrush, dol);
+                dataSet[x, y] = STONE.white;
+
                 isBlack = true;
             }
+
+            //오목 여부 판정
+            CheakOmok(x, y);
+        }
+
+        //8방향 체크
+        public void CheakOmok(int x, int y)
+        {
+            int cBlack = 1;
+            int cWhite = 1;
+
+            /*
+            // ↑
+            for (int i = y - 1; i >= 0; i--)
+            {
+                if (dataSet[i, y] == dataSet[x, y])
+                    count++;
+                else
+                    break;
+            }
+
+            // ↓
+            for (int i = y + 1; i <= 18; i++)
+            {
+                if (dataSet[i, y] == dataSet[x, y])
+                    count++;
+                else
+                    break;
+            }
+
+            //카운트 5 달성 시 오목
+            if (count >= 5)
+            {
+                MessageBox.Show("오목입니다.");
+                return;
+            }
+            count = 1;
+
+            // →
+            for (int i = x + 1; i <= 18; i++)
+            {
+                if (dataSet[i, y] == dataSet[x, y])
+                    count++;
+                else
+                    break;
+            }
+
+            // ←
+            for (int i = x - 1; i >= 0; i--)
+            {
+                if (dataSet[i, y] == dataSet[x, y])
+                    count++;
+                else
+                    break;
+            }
+
+            //카운트 5 달성 시 오목
+            if (count >= 5)
+            {
+                MessageBox.Show("오목입니다.");
+                return;
+            }
+            count = 1;
+
+            // ↗
+            for (int i = x + 1, j = y - 1; i <= 18 && j >= 0; i++, j--)
+            {
+                if (dataSet[i, y] == dataSet[x, y])
+                    count++;
+                else
+                    break;
+            }
+
+            // ↘
+            for (int i = x + 1, j = y + 1; i <= 18 && j <= 18; i++, j++)
+            {
+                if (dataSet[i, y] == dataSet[x, y])
+                    count++;
+                else
+                    break;
+            }
+
+            //카운트 5 달성 시 오목
+            if (count >= 5)
+            {
+                MessageBox.Show("오목입니다.");
+                return;
+            }
+            count = 1;
+
+            // ↙
+            for (int i = x - 1, j = y + 1; i >= 0 && j <= 18; i--, j++)
+            {
+                if (dataSet[i, y] == dataSet[x, y])
+                    count++;
+                else
+                    break;
+            }
+
+            // ↖
+            for (int i = x - 1, j = y - 1; i >= 0 && j >= 0; i--, j--)
+            {
+                if (dataSet[i, y] == dataSet[x, y])
+                    count++;
+                else
+                    break;
+            }
+
+            //카운트 5 달성 시 오목
+            if (count >= 5)
+            {
+                MessageBox.Show("오목입니다.");
+                return;
+            }
+            count = 1;
+            */
+        }
+
+        public void NewGame()
+        {
+            //화면 갱신 -> OnPaint()호출
+            this.Invalidate(); 
+            //데이터셋 초기화
+            for (int x = 0; x < 19; x++)
+                for (int y = 0; y < 19; y++)
+                    dataSet[x, y] = STONE.none;
+            //상태변수 초기화
+            isBlack = true;
         }
 
         protected override void OnPaint(PaintEventArgs e)
