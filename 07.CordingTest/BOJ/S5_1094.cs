dea4da1a7397969ff834c051d147cbd0e203bdf9
Ugee99 <shu990810@naver.com>
2022-11-01 18:09:07 +0900

    .

---
+++ 07.CordingTest/BOJ/S5_1094.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Study._07.CordingTest.BOJ
+{
+    internal class S5_1094
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
